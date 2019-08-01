ae4618f1a60cbcd493be0e1baaf81952607cc639
fan <blackeyesfan@gmail.com>
2019-08-01 11:20:08 +0800

    发布版本1.0.19,测试172.18.106.206

--- paysdk/src/main/java/com/hltx/lamic/lamicpay/bean/MethodConfig.java
+++ paysdk/src/main/java/com/hltx/lamic/lamicpay/bean/MethodConfig.java
@@ -11,8 +11,9 @@ public class MethodConfig {
     /**
      * 正式环境
      */
-    private final static String HOST_GETE                    = "gateway.lamic.cn";
-    public final static String BASE_URL_GETE                 = "https://" + HOST_GETE + "/";
+//    private final static String HOST_GETE                    = "gateway.lamic.cn";
+    private final static String HOST_GETE                    = "172.18.106.206";
+    public final static String BASE_URL_GETE                 = "http://" + HOST_GETE + "/";
     /**
      * 测试环境
      */
